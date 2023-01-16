aca65c56ad71130a0d80ddff1cecf2f3cef6db86
wa101 <wadhah.mahroug15@gmail.com>
2023-01-16 18:56:47 +0100

    work

--- modules/argocd/helm.tf
+++ modules/argocd/helm.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "argocd" {
-  name       = "istio-base"
+  name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   namespace  = kubernetes_namespace.namespace.metadata[0].name
--- modules/k8s-engine/azure/cluster.tf
+++ modules/k8s-engine/azure/cluster.tf
@@ -30,9 +30,4 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     network_plugin = "kubenet"
     network_policy = "calico"
   }
-
-  azure_active_directory_role_based_access_control {
-    managed            = true
-    azure_rbac_enabled = true
-  }
 }
