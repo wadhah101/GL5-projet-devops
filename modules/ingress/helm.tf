51eaa1b5c00da901334531a17691d973e9bcdb0b
wa101 <wadhah.mahroug15@gmail.com>
2023-01-16 19:32:13 +0100

    added ingress

---
+++ modules/ingress/helm.tf
@@ -0,0 +1,6 @@
+resource "helm_release" "ingress" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = kubernetes_namespace.namespace.metadata[0].name
+}
