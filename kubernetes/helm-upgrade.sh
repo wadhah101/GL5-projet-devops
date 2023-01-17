733c4809f50b0b204753b7a907a2cf3693c1aa97
Mellywins <ozouaghi@trumidtech.com>
2023-01-17 13:30:52 +0100

    images tags updated

--- kubernetes/helm-upgrade.sh
+++ kubernetes/helm-upgrade.sh
@@ -1,5 +1,4 @@
-helm upgrade -f services/api-gateway/values.yaml -n backend api-gateway-release node/node-app-chart
-
-helm upgrade -f services/order/values.yaml -n backend order-release node/node-app-chart
-
-helm upgrade -f services/payment/values.yaml -n backend payment-release node/node-app-chart
+#!/bin/bash
+helm upgrade -f services/api-gateway/values.yaml -n backend api-gateway-release node/node-app-chart &&
+    helm upgrade -f services/order/values.yaml -n backend order-release node/node-app-chart &&
+    helm upgrade -f services/payment/values.yaml -n backend payment-release node/node-app-chart
