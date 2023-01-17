f9463da3420622128d85a44a3c19087465f72fc4
wa101 <wadhah.mahroug15@gmail.com>
2023-01-17 12:21:58 +0100

    add mongo password

--- modules/prometheus-stack/helm.tf
+++ modules/prometheus-stack/helm.tf
@@ -3,4 +3,8 @@ resource "helm_release" "kube_prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
   namespace  = kubernetes_namespace.namespace.metadata[0].name
+
+  values = [
+    "${file("${path.module}/values.yaml")}"
+  ]
 }
